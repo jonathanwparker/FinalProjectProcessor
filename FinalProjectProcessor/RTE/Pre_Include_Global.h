c7afb8e15826f5a037c8af580ce0484e9f2f038d
jonathanwparker <jonathanwparker1999@gmail.com>
2023-10-25 21:59:55 -0500

    First Draft of Index/Init Code

--- FinalProjectProcessor/RTE/Pre_Include_Global.h
+++ FinalProjectProcessor/RTE/Pre_Include_Global.h
@@ -2,8 +2,8 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: Lab_11
- * RTE configuration: Lab_11.rteconfig
+ * Project: FinalProjectProcessor
+ * RTE configuration: FinalProjectProcessor.rteconfig
 */
 #ifndef PRE_INCLUDE_GLOBAL_H
 #define PRE_INCLUDE_GLOBAL_H
