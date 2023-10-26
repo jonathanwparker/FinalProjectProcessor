22576141023a3f07049524fde21e07417ae6fc35
jonat <jonat@WIN-O8654BPKTN2>
2023-10-25 21:58:59 -0500

    First Draft of Indexing Code

--- FinalProjectProcessor/RTE/RTE_Components.h
+++ FinalProjectProcessor/RTE/RTE_Components.h
@@ -2,8 +2,8 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: Lab_11
- * RTE configuration: Lab_11.rteconfig
+ * Project: FinalProjectProcessor
+ * RTE configuration: FinalProjectProcessor.rteconfig
 */
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
